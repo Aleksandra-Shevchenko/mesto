532ab3c45d607dbaa6891d594a962540a1743c49
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2020-10-23 18:20:58 +0300

    Внесены исправления после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,13 @@ let container = document.querySelector('.container');
 let editButton = container.querySelector('.profile__edit-btn');
 let profileNameElement = container.querySelector('.profile__title');
 let profileJobElement = container.querySelector('.profile__subtitle');
-let popup = container.querySelector('.popup');
+
+let popup = document.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
 let profileNameInput = popup.querySelector('.popup__input_text_name');
 let profileJobInput = popup.querySelector('.popup__input_text_job');
 let formElement = popup.querySelector('.popup__form');
-let formButton = popup.querySelector('.submit-btn');
+
 
 function showPopup () {
   popup.classList.add('popup_opened');
@@ -19,17 +20,15 @@ function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function handlePopup (evt) {
     evt.preventDefault();
     profileNameElement.textContent = profileNameInput.value;
     profileJobElement.textContent = profileJobInput.value;
+    closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-formButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handlePopup);
 
 editButton.addEventListener('click', showPopup);
 
 popupClose.addEventListener('click', closePopup);
-
