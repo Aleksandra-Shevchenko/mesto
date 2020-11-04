6b6979a3e8d7e1194335e62168840ed52b0fbd6e
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2020-11-04 14:12:08 +0300

    Карточки добавляются JS при загрузке страницы

--- scripts/index.js
+++ scripts/index.js
@@ -32,11 +32,11 @@ function handlePopup(evt) {
 
 // функция обработки клика за пределами области popup__container
 function handlePopupClick(evt) {
+  console.log(evt.target);
   if (evt.target.classList.contains('popup')){
     closePopup();
   }
 }
-
 
 formElement.addEventListener('submit', handlePopup);
 
@@ -45,3 +45,61 @@ editButton.addEventListener('click', showPopup);
 popupClose.addEventListener('click', closePopup);
 
 popup.addEventListener('mousedown', handlePopupClick);
+
+
+// Проектная работа №5
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardContainer = document.querySelector('.elements'); //контейнер куда будем вставлять карточки
+
+// Функция создания карточки должна решать только одну задачу - создавать DOM элемент карточки, но не вставлять её сразу в контейнер. Функция createCard: создает карточку из темплейта, подставляет данные, навешивает обработчики и возращает созданную карточку.
+
+
+// функция создания карточки с фото, она принимает объект с данными карточки
+function createCard(itemArr) {
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImg = cardElement.querySelector('.element__pic');
+  const cardTitle = cardElement.querySelector('.element__title');
+
+  cardTitle.textContent = itemArr.name;
+  cardImg.src = itemArr.link;
+  cardImg.alt = (`Фото ${itemArr.name}`);
+
+  return cardElement;
+}
+
+
+//на вход получаем объект из массива, с каждым объектом создаем DOM элемент картчоки и вставляем полученный элемент в контейнер карточек
+initialCards.forEach(function (item) {
+  const newCard = createCard(item);
+  // console.log (createCard(item));
+  cardContainer.prepend(newCard);
+})
+
