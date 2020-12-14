9a2bca27755427087836beb75135b946f6b9e1d1
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2020-12-14 09:24:45 +0300

    changed position of the constants

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-// --- ДАННЫЕ И ПЕРЕМЕННЫЕ ---
+// --- ДАННЫЕ  ---
 
 //объект параметров для валидации форм
 const validationObject = {
@@ -38,50 +38,9 @@ const initialCards = [
   }
 ];
 
-const container = document.querySelector('.container');
-const cardContainer = container.querySelector('.elements');
-
-//элементы секции профайл
-const profileNameElement = container.querySelector('.profile__title');
-const profileJobElement = container.querySelector('.profile__subtitle');
-const editButton = container.querySelector('.profile__edit-btn');
-const addPhotoButton = container.querySelector('.profile__add-btn');
-
-//элементы попапа для редактирования профайла
-const popupProfile = document.querySelector('.popup_type_edit');
-const formElementProfile = popupProfile.querySelector('.popup__form');
-const profileNameInput = popupProfile.querySelector('.popup__input_text_name');
-const profileJobInput = popupProfile.querySelector('.popup__input_text_job');
-
-//элементы попапа для добавления фото
-const popupAddCard = document.querySelector('.popup_type_add-card');
-const formElementAddCard = popupAddCard.querySelector('.popup__form');
-const formInputCardName = popupAddCard.querySelector('.popup__input_add-card_name');
-const formInputCardLink = popupAddCard.querySelector('.popup__input_add-card_link');
-
-//элементы попапа с большим фото
-const popupImage = document.querySelector('.popup_type_image');
-const bigPhotoPopupImage = popupImage.querySelector('.popup__photo');
-const popupImageTitle = popupImage.querySelector('.popup__photo-title');
 
 
 export {
   validationObject,
   initialCards,
-  cardContainer,
-  profileNameElement,
-  profileJobElement,
-  editButton,
-  addPhotoButton,
-  popupProfile,
-  formElementProfile,
-  profileNameInput,
-  profileJobInput,
-  popupAddCard,
-  formElementAddCard,
-  formInputCardName,
-  formInputCardLink,
-  popupImage,
-  bigPhotoPopupImage,
-  popupImageTitle,
 };
